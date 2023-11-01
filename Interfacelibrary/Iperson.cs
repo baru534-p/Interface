31d3e70f2e2786fd70f574ca2d41821a7db11682
TavvaBhargavi <baru.tavva@gmail.com>
2023-11-01 14:43:06 +0530

    Add project files.

---
+++ Interfacelibrary/Iperson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfacelibrary
+{
+    public class Iperson
+    {
+        public interface IPerson
+        {
+            string getInfo();
+            string getTypeOfPerson();
+        }
+    }
+
+}
+
